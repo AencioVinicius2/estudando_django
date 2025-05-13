503a1c5c7953d4b521413b4e8d64f8642a27e2e7
Aencio <aenpvp9@gmail.com>
2025-05-13 19:36:00 -0300

    feat: dailycommoit

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def my_view(request):
+    return HttpResponse('UMA LINDA STRING')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view),
+    path(''),
 ]
