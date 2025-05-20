ca65a0c778156b4f1cc90f1982b5b0f207efbcd7
Aencio <aenpvp9@gmail.com>
2025-05-20 14:38:03 -0300

    feat: partials created

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 # Create your views here.
 def home(request):
-    return render(request,'recipes/home.html')
+    return render(request,'recipes/pages/home.html')
 
