effa9c3cdaf400fdacde910f775210fa62aa2551
Aencio <aenpvp9@gmail.com>
2025-05-20 11:46:29 -0300

    removed contato and bancos

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-
-from recipes.views import home, contato, sobre
+from recipes.views import home
 
 urlpatterns = [
     path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
 # Create your views here.
-print('Acessar site em http://127.0.0.1:8000/contato/')
-
 def home(request):
     return render(request,'recipes/home.html')
 
-def contato(request):
-    return render(request,'temp/temp.html')
-
-def sobre(request):
-    return HttpResponse('VOCÊ ESTÁ EM SOBRE')
